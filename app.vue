f7ef849d4ea49012965f34d020f4d82052716b4c
Aleskey <colthands@protonmail.com>
2023-09-11 21:52:38 +0400

    UPDATE: Bacic setup + add tailwind and eslint

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
+
+<script lang="ts">
+useHead({
+    title: 'Blockchain Developer Stats',
+    meta: [
+        {
+            hid: 'description',
+            name: 'description',
+            content: 'Blockchain Developer Stats'
+        }
+    ]
+})
+</script>
+
+<style lang="sass">
+@use '~~/assets/styles/variables.sass'
+
+html, body
+    background: variables.$bgc
+    background-repeat: no-repeat
+    background-size: contain
+    width: 100%
+    height: 100%
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { NuxtConfig, defineNuxtConfig } from 'nuxt/config'
+
+type NuxtConfigExtended = NuxtConfig & {
+    tailwindcss: Record<string, unknown>
+}
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss'],
+    tailwindcss: {},
+    sass: ['~/assets/styles/main.sass']
+} as NuxtConfigExtended)
