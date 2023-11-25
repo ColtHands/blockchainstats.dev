3a83b2906a9240753f7e44be32b8f9da328fd609
Aleskey <colthands@protonmail.com>
2023-11-25 23:09:35 +0400

    [feature] Add links, icons and animation to header

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -5,13 +5,16 @@
                 <i class="fa-solid fa-cube"></i> BDS
             </NuxtLink>
             <HeaderSearchBar class="w-72"></HeaderSearchBar>
+            <!-- TODO Add those animations to MyNuxtLink component -->
             <nav class="flex justify-center gap-4">
-                <div>Projects</div>
-                <div>About</div>
-                <div>Tags</div>
+                <NuxtLink class="hover:text-green-400 transition-colors" to="/projects">Projects</NuxtLink>
+                <NuxtLink class="hover:text-green-400 transition-colors" to="/about">About</NuxtLink>
+                <NuxtLink class="hover:text-green-400 transition-colors" to="/tags">Tags</NuxtLink>
+                <NuxtLink class="hover:text-green-400 transition-colors" to="/coins">Coins</NuxtLink>
             </nav>
-            <div>
-                social icons
+            <div class="flex justify-center gap-4">
+                <NuxtLink class="hover:text-green-400 transition-colors" to="github.com"><i class="fa-brands fa-github"></i></NuxtLink>
+                <NuxtLink class="hover:text-green-400 transition-colors" to="twitter.com"><i class="fa-brands fa-x-twitter"></i></NuxtLink>
             </div>
         </div>
     </header>
