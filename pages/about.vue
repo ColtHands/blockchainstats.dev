12c868344bc357e6c5ca8257bda6117cefb46b55
Aleskey <colthands@protonmail.com>
2023-11-27 22:09:34 +0400

    [feature] Add about with some lorem ipsum type text

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,9 @@
 <template>
-    about page
+    <div>
+        about page
+        <!-- <div>this project is blah blah</div> -->
+        <!-- <div>project roadmap</div> -->
+        <!-- <div>i sometimes tweet about stats</div> -->
+        <!-- <div>come tell me suggestions</div> -->
+    </div>
 </template>
