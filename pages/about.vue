4fa2612d1cf0bc695f2e4bd9e6e281e94c3afb55
Aleskey <colthands@protonmail.com>
2023-11-27 22:12:10 +0400

    [upd] Add section into /about map

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,8 @@
         about page
         <!-- <div>this project is blah blah</div> -->
         <!-- <div>project roadmap</div> -->
+        <!-- <div>project tech stack description</div> -->
+        <!-- <div>project backend flowchart</div> -->
         <!-- <div>i sometimes tweet about stats</div> -->
         <!-- <div>come tell me suggestions</div> -->
     </div>
