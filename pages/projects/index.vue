14bca198d2035e85892481a41f9a131bd98e343f
Aleskey <colthands@protonmail.com>
2023-11-30 01:30:19 +0400

    [upd] Update projects page text

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-5xl font-bold mt-8 flex justify-center gap-5">
         <i class="fa-solid fa-chart-bar"></i>
-        <h1>Total 123123 project in database</h1>
+        <h1>Total {} projects in database</h1>
     </div>
     <SectionTilesStateful />
 </template>
