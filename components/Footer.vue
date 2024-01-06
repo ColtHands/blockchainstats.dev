d055c363908ec1a1bf383ae1f808113ebc2033a0
Aleskey <colthands@protonmail.com>
2024-01-06 21:30:10 +0400

    [feat] Responsify <Footer>

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
     <footer class="border-t border-cyan-950">
-        <div class="mx-auto py-8 grid grid-cols-3 items-center text-center w-full sm:w md:w-3/3 lg:w-2/4 xl:w-3/5">
-            <NuxtLinkExtended to="/">
+        <div class="mx-auto py-4 sm:py-8 grid grid-cols-3 items-center text-center w-full lg:w-2/4 xl:w-3/5">
+            <NuxtLinkExtended to="/" class="max-sm:hidden">
                 <i class="fa-solid fa-cube text-green-400 text-5xl"></i>
             </NuxtLinkExtended>
-            <nav>
+            <nav class="max-sm:col-span-3">
                 <p class="text-xs text-gray-500">Links</p>
                 <ul class="flex justify-center gap-3 mt-1 text-sm">
                     <li v-for="{title,path} in useAppConfig().navigationLinks" :key="path">
@@ -14,12 +14,12 @@
                     </li>
                 </ul>
             </nav>
-            <div class="flex justify-center gap-2">
-                <NuxtLinkExtended to="https://github.com/ColtHands/blockchain-developer-stats-frontend" class="text-2xl"><i class="fa-brands fa-github"></i></NuxtLinkExtended>
-                <NuxtLinkExtended to="https://twitter.com/0xColtHands" class="text-2xl"><i class="fa-brands fa-x-twitter"></i></NuxtLinkExtended>
+            <div class="flex justify-center gap-2 text-xl xs:text-2xl max-sm:col-span-3 max-sm:mt-4">
+                <NuxtLinkExtended to="https://github.com/ColtHands/blockchain-developer-stats-frontend"><i class="fa-brands fa-github"></i></NuxtLinkExtended>
+                <NuxtLinkExtended to="https://twitter.com/0xColtHands"><i class="fa-brands fa-x-twitter"></i></NuxtLinkExtended>
             </div>
         </div>
-        <div class="border-t border-cyan-950 pt-4 pb-2 text-center text-gray-500 text-sm">
+        <div class="border-t border-cyan-950 pt-4 pb-2 text-center text-gray-500 text-sm max-xs:text-xs">
             Built by Aleksei Karpenko
             •
             <NuxtLink to="https://colthands.dev" class="text-green-400">colthands.dev</NuxtLink>
