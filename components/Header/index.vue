c41d3bf7104e1af7b503ed55b2a244493ec697cf
Aleskey <colthands@protonmail.com>
2024-01-06 21:22:03 +0400

    [feat] Resposify <Header>

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -2,12 +2,12 @@
     <header class="py-6 w-full backdrop-blur border-b border-cyan-950">
         <div class="sm:container sm:mx-auto px-4 flex justify-between items-center">
             <NuxtLinkExtended class="font-bold text-2xl" :title="runtimeConfig.public.title" to="/">
-                <i class="fa-solid fa-cube"></i> BDS
+                <i class="fa-solid fa-cube"></i> <span class="max-sm:hidden">BDS</span>
             </NuxtLinkExtended>
 
-            <HeaderSearchBar class="w-72"></HeaderSearchBar>
+            <HeaderSearchBar class="max-md:hidden md:w-72"></HeaderSearchBar>
 
-            <nav class="flex justify-center gap-4 text-sm">
+            <nav class="flex justify-center gap-2 md:gap-4 text-sm">
                 <NuxtLinkExtended
                     v-for="{title,path} in useAppConfig().navigationLinks"
                     :key="path"
@@ -17,9 +17,9 @@
                 </NuxtLinkExtended>
             </nav>
 
-            <div class="flex justify-center gap-4">
-                <NuxtLinkExtended to="https://github.com/ColtHands/blockchain-developer-stats-frontend" class="text-xl"><i class="fa-brands fa-github"></i></NuxtLinkExtended>
-                <NuxtLinkExtended to="https://twitter.com/0xColtHands/" class="text-xl"><i class="fa-brands fa-x-twitter"></i></NuxtLinkExtended>
+            <div class="flex justify-center gap-2 md:gap-4 text-xl">
+                <NuxtLinkExtended to="https://github.com/ColtHands/blockchain-developer-stats-frontend"><i class="fa-brands fa-github"></i></NuxtLinkExtended>
+                <NuxtLinkExtended to="https://twitter.com/0xColtHands/"><i class="fa-brands fa-x-twitter"></i></NuxtLinkExtended>
             </div>
         </div>
     </header>
