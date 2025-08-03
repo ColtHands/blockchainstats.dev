a79db5ecf1939c850402dd63bcea16bdb4f039bd
Aleksei <aleksei@jslead.dev>
2025-08-04 00:52:37 +0400

    [chore] Update eslint config version

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import myConfig from '@coldhands/eslint-config'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+    ...myConfig,
+])
--- utils/filterOutEmptyStrings.ts
+++ utils/filterOutEmptyStrings.ts
@@ -2,6 +2,7 @@
  * In coingecko API links come as @type {Array<string>}, but some of the string may be empty
  * This function filters out empty string
  * @example filterOutEmptyStrings(["bitcoin.com", "", ""])
+ * @param {Array<string>} arr
  * @returns ["bitcoin.com"]
  */
 export default function filterOutEmptyStrings(arr: Array<string>): Array<string> {
